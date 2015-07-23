cdcfa2dc1c59049ee0bb2b5acf56d4b051684a0f
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-07-23 16:17:24 +0200

    Adding training functions for upper-level policies

--- bolero_bayes_opt/representation/ul_policies.py
+++ bolero_bayes_opt/representation/ul_policies.py
@@ -1,8 +1,13 @@
 import numpy as np
-from bolero.representation.ul_policies import UpperLevelPolicy
-from bolero.utils.validation import check_random_state
+
 from sklearn.kernel_approximation import Nystroem
 from sklearn.linear_model import Ridge
+
+from bolero.representation.ul_policies \
+    import UpperLevelPolicy, BoundedScalingPolicy
+from bolero.utils.validation import check_random_state
+
+from bayesian_optimization import GaussianProcessModel
 
 
 class KernelRegressionPolicy(UpperLevelPolicy):
@@ -149,3 +154,211 @@ class KernelRegressionPolicy(UpperLevelPolicy):
         self.W = ridge.coef_
 
         # TODO: self.Sigma needs to be adapted
+
+
+def model_free_policy_training(policy, contexts, parameters, returns=None,
+                               epsilon=1.0, min_eta=1e-6):
+    """Train a contextual policy in a model-free way using REPS.
+
+    Use the given (context, parameter, return) triples for training a
+    contextual policy using REPS. The contexts are passed through a
+    context_transformation function.
+
+    Parameters
+    ----------
+    policy: UpperLevelPolicy
+        A contextual upper level policy that shall be updated based on samples.
+
+    contexts: array-like, [n_samples, context_dims]
+        The contexts used for training
+
+    parameters: array-like, [n_samples, param_dims]
+        The parameters of the low-level policy used in the respective context.
+
+    returns: array-like, [n_samples]
+        The returns obtained by the low-level policy in the respective context.
+        If None, then all training examples are used with equal weight;
+        otherwise the returns are used to determine the weights of the samples
+        by using REPS.
+
+    epsilon: float > 0.0
+        The maximum the KL divergence between training and policy distribution
+        might take on. Defaults to 1.0
+
+    min_eta: float > 0.0
+        eta is the Lagrangian multiplier connected to the KL-bound constraint.
+        If it becomes too small, numerical instability might occur. Because of
+        this, min_eta can be set to a value larger than zero which acts as a
+        lower bound for eta. Defaults to 1e-6.
+
+    Returns
+    -------
+    policy : LinearGaussianPolicy
+        Upper level policy that maps contexts to parameter vectors
+    """
+    parameters = np.asarray(parameters)
+    features = np.array([policy.transform_context(c) for c in contexts])
+    if returns is not None:
+        returns = np.asarray(returns)
+        from optimizer.python.utils.reps import solve_dual_contextual_reps
+        d, _ = solve_dual_contextual_reps(features, returns, epsilon=epsilon,
+                                          min_eta=min_eta)
+    else:
+        d = np.ones(contexts.shape[0]) / contexts.shape[0]
+
+    policy.fit(contexts, parameters, d)
+
+    return policy
+
+
+def model_based_policy_training(policy, contexts, parameters, returns,
+                                boundaries=None, policy_initialized=False,
+                                model_conf={}, maxfun=5000,
+                                variance=0.01, *args, **kwargs):
+    """Train a contextual policy in a model-based way on GP model.
+
+    Use the given (context, parameter, return) triples for training a
+    model (based on Gaussian processes) of the function
+    context x parameter > return. Optimize the parameters of a parametric
+    policy such that it would maximize the accrued return in the model.
+
+    Parameters
+    ----------
+    policy: UpperLevelPolicy
+        A contextual upper level policy that shall be updated based on samples.
+
+    contexts: array-like, [n_samples, context_dims]
+        The contexts used for training
+
+    parameters: array-like, [n_samples, param_dims]
+        The parameters of the low-level policy used in the respective context.
+
+    returns: array-like, [n_samples]
+        The returns obtained by the low-level policy in the respective context.
+        If None, then all training examples are used with equal weight;
+        otherwise the returns are used to determine the weights of the samples
+        by using REPS.
+
+    boundaries : array-like or None, shape=[param_dims, 2] (default: None)
+        The boundaries of the action space.
+
+    policy_initialized : bool (default: False)
+        Whether the policy has already been initialized reasonably or needs to
+        be initialized first before performing model-based optimization
+
+    model_conf: dict (default : {})
+        Passed as keyword arguments to the GP model.
+
+    maxfun: int, optional (default: 5000)
+        The maximal number of policy parameters after which the optimizer
+        terminates
+
+    variance: float, optional (default: 0.01)
+        The initial exploration variance of CMA-ES
+
+    Returns
+    -------
+    policy : LinearGaussianPolicy
+        Upper level policy that maps contexts to parameter vectors
+    """
+    contexts = np.asarray(contexts)
+    parameters = np.asarray(parameters)
+
+    # Train model approximating the context x parameter -> return landscape
+    inputs = np.hstack([contexts, parameters])
+    model = GaussianProcessModel(**model_conf)
+    model.fit(inputs, returns)
+
+    if not policy_initialized:
+        # Initialize policy
+        if boundaries is not None:
+            # Initialize policy such that outputs are maximally far from
+            # boundary
+            policy.fit(contexts, [boundaries.mean(1)]*contexts.shape[0],
+                       weights=np.ones(contexts.shape[0]))
+        else:
+            # Let policy return mean parameter vector
+            policy.fit(contexts, parameters.mean(),
+                       weights=np.ones(contexts.shape[0]))
+
+    return model_based_policy_training_pretrained(
+        policy, model.gp, contexts, boundaries, maxfun, variance,
+        *args, **kwargs)
+
+
+def model_based_policy_training_pretrained(
+     policy, model, contexts, boundaries=None, maxfun=5000, variance=0.01,
+     *args, **kwargs):
+    """Train a contextual policy in a model-based way on GP model.
+
+    Optimize the parameters of a parametric policy such that it would maximize
+    the accrued return in a pretrained model for the given contexts.
+
+    Parameters
+    ----------
+    policy: UpperLevelPolicy
+        A contextual upper level policy that shall be updated based on samples.
+
+    model: GPR-instance
+        The GPR-model in which the policy is optimized
+
+    contexts: array-like, [n_samples, context_dims]
+        The contexts used for training
+
+    boundaries : array-like or None, shape=[param_dims, 2] (default: None)
+        The boundaries of the action space.
+
+    maxfun: int, optional (default: 5000)
+        The maximal number of policy parameters after which the optimizer
+        terminates
+
+    variance: float, optional (default: 0.01)
+        The initial exploration variance of CMA-ES
+
+    Returns
+    -------
+    policy : LinearGaussianPolicy
+        Upper level policy that maps contexts to parameter vectors
+    """
+
+    def average_return(policy_params):
+        """ Return predicted return of policy for given policy parameters. """
+        # Instantiate policy
+        policy.W = np.array(policy_params.reshape(policy.W.shape))
+        policy.W *= scale_factor
+        # Determine parameters selected by policy for given contexts
+        params = policy(contexts, explore=False)
+        if boundaries is not None:  # check boundaries
+            params = np.minimum(np.maximum(params, boundaries[:, 0]),
+                                boundaries[:, 1])
+        # Compute mean output of GP model for contexts and selected params
+        values = model.predict(np.hstack((contexts, params)))
+        # Return mean over return obtained on all training contexts
+        return values.mean()
+
+    if boundaries is not None:
+        # Compute scale factor per dimension such that isotropic exploration
+        # is meaningful
+        if isinstance(policy, BoundedScalingPolicy):
+            scale_factor = \
+                (policy.scaling.inv_scale(boundaries[:, 1])
+                 - policy.scaling.inv_scale(boundaries[:, 0]))[:, None]
+        else:
+            scale_factor = (boundaries[:, 1] - boundaries[:, 0])[:, None]
+    else:
+        scale_factor = 1  # Don't do scaling since the scales are unknown
+
+    # Refine policy determined in model-free way by performing L-BFGS on
+    # the model.
+    from bolero.optimizer.cmaes import fmin as fmin_cmaes
+    policy.W /= scale_factor
+    x_lbfgs, _ = \
+        fmin_cmaes(average_return, x0=policy.W.flatten(), maxfun=maxfun,
+                   eval_initial_x=True, variance=variance, maximize=True,
+                   *args, **kwargs)
+
+    # Set weights of linear policy
+    policy.W = x_lbfgs.reshape(policy.W.shape)
+    policy.W *= scale_factor
+
+    return policy

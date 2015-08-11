be7491cfe0a7e20eec5ae0b113f62a782d9c8d5d
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-08-11 08:27:38 +0200

    ADD Adding intial version of ACEPSOptimizer

--- bolero_bayes_opt/__init__.py
+++ bolero_bayes_opt/__init__.py
@@ -1,3 +1,4 @@
 from .optimizer.bops import BOPSOptimizer
 from .optimizer.bocps import BOCPSOptimizer
+from .optimizer.aceps import ACEPSOptimizer
 from .representation.ul_policies import KernelRegressionPolicy
